70491a6d1f628f5e75db13519483392aa2f13ef7
1caldeira <g.caldeiraa1@gmail.com>
2024-09-02 17:03:29 -0300

    OrderItem, many-to-many association with extra attributes

--- course/src/main/java/com/educandoweb/course/entities/Order.java
+++ course/src/main/java/com/educandoweb/course/entities/Order.java
@@ -2,18 +2,14 @@ package com.educandoweb.course.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import com.educandoweb.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity 
 @Table(name = "tb_order")
@@ -32,10 +28,12 @@ public class Order implements Serializable {
 	private User client;
 
 	private Integer orderStatus;
-	
+
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
 	public Order() {}
 
-	public Order(Long id, Instant moment, User client, OrderStatus orderStatus) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
@@ -75,6 +73,10 @@ public class Order implements Serializable {
 
 	public void setClient(User client) {
 		this.client = client;
+	}
+
+	public Set<OrderItem> getItems(){
+		return items;
 	}
 
 	@Override
